6ee1d154133c091d8870ff2a4e3ca34190c93b7e
BJ Homer <bjhomer@gmail.com>
2012-10-19 12:49:35 -0600

    Add Demo project, showing how to use RefreshControl.

---
+++ RefreshControlDemo/ColorRefreshController.m
@@ -0,0 +1,88 @@
+// ColorRefreshController.m
+//
+// Copyright (c) 2012 Instructure, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "ColorRefreshController.h"
+#import "CKRefreshControl.h"
+
+@implementation ColorRefreshController {
+    NSDictionary *colors;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    colors = @{
+    @"Orange" : [UIColor orangeColor],
+    @"Red" : [UIColor redColor],
+    @"Blue" : [UIColor blueColor],
+    @"Gray" : [UIColor grayColor],
+    @"Black" : [UIColor blackColor],
+    };
+    
+    CKRefreshControl *refreshControl = [CKRefreshControl new];
+    refreshControl.tintColor = [UIColor orangeColor];
+    [refreshControl addTarget:self action:@selector(doRefresh:) forControlEvents:UIControlEventValueChanged];
+    self.refreshControl = (id)refreshControl;
+}
+
+- (void)doRefresh:(CKRefreshControl *)sender {
+    NSLog(@"refreshing");
+    [self.refreshControl performSelector:@selector(endRefreshing) withObject:nil afterDelay:1.0];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return colors.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Configure the cell...
+    cell.textLabel.text = [colors allKeys][indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    NSArray *colorNames = [colors allKeys];
+    NSString *selectedName = colorNames[indexPath.row];
+    self.refreshControl.tintColor = colors[selectedName];
+}
+
+@end
